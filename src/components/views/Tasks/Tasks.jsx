3044b4327b4e5c8ce2e7e07d03d2cbd6678e04d2
Devi Amaolo <deviamaolo@gmail.com>
2022-08-25 03:09:21 -0300

    custom hook useResize created

--- src/components/views/Tasks/Tasks.jsx
+++ src/components/views/Tasks/Tasks.jsx
@@ -1,24 +1,21 @@
+import './Task.styles.css'
+import { useResize } from '../../../hook/useResize'
 import { Header } from  '../../Header/Header'
-import { useEffect, useState } from 'react'
+import { cardsData } from './'
+import { Card } from '../../Card/Card'
 
 export const Tasks = () => {
-  const [isPhone, setIsPhone] = useState(window.innerWidth < 900 ? true : false)
 
-  const handleResize = () => {
-    if(window.innerWidth < 900) setIsPhone(true)
-    else setIsPhone(false)
-  }
-
-  useEffect(() => {
-    handleResize()
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
-  })
+  const { isPhone } = useResize()
 
   const limitString = (str) => {
     if(str.length > 370)
       return { string: str.slice(0,367).concat('...'), addButton: true }
     return { string: str, addButton: false}
+  }
+
+  const renderAllCards = () => {
+    return cardsData.map(data => <Card key={data.id} data={data}/>)
   }
 
   return (
@@ -29,17 +26,9 @@ export const Tasks = () => {
           <div className='list_header'>
             <h2>My Task</h2>
           </div>
-          {isPhone ? (<div className='list phone'>
-            <h4>Finished</h4>
-              <div className='card'>
-                <div className='close'>x</div>
-                <h3>Task 1</h3>
-                <h6>24/1/2022 16:40 hs.</h6>
-                <h5>Devi Amaolo</h5>
-                <button type='button'>New</button>
-                <button type='button'>Alta</button>
-                <p>{limitString(`fake description`).string}</p>
-              </div>
+          {isPhone ? (
+            <div className='list phone'>
+              {renderAllCards()}
             </div>) : (<div className='list_group'>
             <div className='list'>
               <h4>news</h4>
